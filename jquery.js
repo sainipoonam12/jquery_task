7777a0558167fb052c323c4ea6424be72bc02ff7
poonam <sainipoonam2015@gmail.com>
2021-11-13 11:50:35 +0530

    first commit

--- jquery.js
+++ jquery.js
@@ -5,30 +5,29 @@ $(document).ready(function(){
         var heading = $(".headingtext").val();
         $("main").append("<div><h1>" + heading + "</h1></div>"); 
         $(".mySelect option").remove();
+        $(".myHeading option").remove();
         $("h1").each(function(index){
             index = index+1;
-            // console.log(index)
             head = $(this).text();
             // $(".mySelect").append($('<option></option>').attr("value",index).text(head));  
             $(".mySelect").append('<option value='+index+'>'+head+'</option>');
-
-            $(".myHeading").append('<option value='+index+'>'+head+'</option>');
-
-            
+            $(".myHeading").append('<option value='+index+'>'+head+'</option>');        
         });
     });
     $(".SecondForm").submit(function(e){
         e.preventDefault();
         var subheading = $(".subheadingtext").val();
         var i = $('.mySelect option:selected').attr('value');
-        console.log(subheading,i)
-        
-        $( "main div:nth-child("+i+")").append("<div><h4>" + subheading + "</h4></div>");   
-        // console.log(i)
+        $("main div:nth-child("+i+")").append("<h4>" + subheading + "</h4>");   
+        $(".mySubheading").append('<option value='+i+'>'+subheading+'</option>');        
+    });    
+    $(".myHeading").on('change',function(){
+        // var selectedheading = $(this).children("option:selected").val();
+        index=$(this).val(); 
+        console.log(index);
+        $(".mySubheading").append('<option value='+index+'></option>');
+    });
 
-
-       
-    });     
 });
 
 
@@ -52,3 +51,6 @@ $(document).ready(function(){
     //     $("#form-div").html(str);
 
     // })
+
+
+    
