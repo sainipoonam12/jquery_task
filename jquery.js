2d8c96191f11ddbe16ae1a181cb978e3ca0c40ff
poonam <sainipoonam12>
2021-11-29 19:16:19 +0530

    jquery

--- jquery.js
+++ jquery.js
@@ -21,22 +21,26 @@ $(document).ready(function () {
         $(".subheadingtext").val('');
         var i = $('.mySelect option:selected').attr('value');
         $("main div:nth-child(" + i + ")").append("<h4>" + subheading + "</h4>");
-        $(".mySubheading").append('<option value=' + i + '>' + subheading + '</option>');
+        // $(".mySubheading").append('<option value=' + i + '>' + subheading + '</option>');
         
     });
     $(".myHeading").on('change', function () {
         var ind = $(this).val();
-        $("main div:nth-child(" + ind + ")").each(function (ind) {
-            console.log(ind)
-            console.log($(this).text());
-            $(".mySubheading option").remove();
-            $(".mySubheading").append('<option value=' + ind + '>' + $(this).text() + '</option>');
+        console.log("vsliurrur",ind)
+        var flag = 0
+        $("main div:nth-child(" + ind + ") h4").each(function (inda) {
+            console.log("dfasdasd",inda)
+            if(flag == 0){
+                $(".mySubheading option").remove();
+                flag = flag+1;
+            }
+            $(".mySubheading").append('<option value=' + inda + '>' + $(this).text() + '</option>');
         })
     });
     $(".ThirdForm").submit(function (r) {
         r.preventDefault();
         var Selectedoption = $('.optn option:selected').text();
-        var fieldSet = $("<fieldset id=\"yourform\"></fieldset>");
+        var fieldSet = $("<fieldset class='your form'></fieldset>");
         var id = $("#field_label").val();
         var label = $("#field_label").val();
         var class_name = $("#field_class").val();
@@ -44,27 +48,32 @@ $(document).ready(function () {
         var field_place = $("#field_place").val();
         var field_options = $("#field_options").val();
         var i = $('.myHeading option:selected').attr('value');
+        var j = $('.mySubheading option:selected').attr('value');
+        console.log("abra ka daba",j)
+        console.log("abr fjflkdjfkla",i)
         var input;
         if(Selectedoption == "Text"){
-            input = $("<input type=\"text\" id=\"" + id + "\" name=\"" + label + "\" class=\"" + class_name + "\" placeholder=\"" + field_place + "\" value=\"" + field_value + "\" />");
+            input = $("<input type='text'  class='" + class_name + "' placeholder='" + field_place + "' value='" + field_value + "' />");
+            console.log(input)
             fieldSet.append(label);
             fieldSet.append(input);
-            $("main div:nth-child("+i+")").append(fieldSet); 
+
+            $("main div:nth-child("+i+") h4:eq("+j+")").append(fieldSet); 
         }
         if(Selectedoption == "Text Area"){
-            input = $("<textarea id=\"" + id + "\" name=\"" + label + "\" class=\"" + class_name + "\" placeholder=\"" + field_place + "\" value=\"" + field_value + "\" />");
+            input = $("<textarea  class='" + class_name + "' placeholder='" + field_place + "' value='" + field_value + "' />");
             fieldSet.append(label);
             fieldSet.append(input);
-            $("main div:nth-child("+i+")").append(fieldSet); 
+            $("main div:nth-child("+i+") h4:eq("+j+")").append(fieldSet); 
         }
         if(Selectedoption == "Number"){
-            input = $("<input type=\"number\" id=\"" + id + "\" name=\"" + label + "\" class=\"" + class_name + "\" placeholder=\"" + field_place + "\" value=\"" + field_value + "\" />");
+            input = $("<input type='number'  class='" + class_name + "' placeholder='" + field_place + "' value='" + field_value + "' />");
             fieldSet.append(label);
             fieldSet.append(input);
-            $("main div:nth-child("+i+")").append(fieldSet); 
+            $("main div:nth-child("+i+") h4:eq("+j+")").append(fieldSet); 
         }
         if(Selectedoption == "Email"){
-            input = $("<input type=\"email\" id=\"" + id + "\" name=\"" + label + "\" class=\"" + class_name + "\" placeholder=\"" + field_place + "\" value=\"" + field_value + "\" />");
+            input = $("<input type='email'  class='" + class_name + "' placeholder='" + field_place + "' value='" + field_value + "' />");
             fieldSet.append(label);
             fieldSet.append(input);
             $("main div:nth-child("+i+")").append(fieldSet); 
@@ -73,11 +82,11 @@ $(document).ready(function () {
             // <label for="vehicle1"> I have a bike</label><br>
             var opts = field_options.split(",");
             opts.map(function(index){
-                // label = index
+                label = index
                 console.log("label",label)
                 console.log("index",index)
                 console.log("id",id)
-                input = $("<input type=\"checkbox\" id=\"" + id + "\" name=\"" + label + "\" class=\"" + class_name + "\" /> <br>");
+                input = $("<input type = 'checkbox'  class='" + class_name + "'   /> <br>");
                 fieldSet.append(label);
                 fieldSet.append(input);
                 $("main div:nth-child("+i+")").append(fieldSet);    
@@ -86,7 +95,8 @@ $(document).ready(function () {
         if(Selectedoption == "Select list") {
             var opts = field_options.split(",");
             opts.map(function(index){
-                input = $("<input type=\"Select list\" id=\"" + id+ "\" name=\"" + label + "\" class=\"" + class_name + "\" /> <br>");
+                label = index
+                input = $("<select class='" + class_name + "' /> <br>");
                 fieldSet.append(label);
                 fieldSet.append(input);
                 $("main div:nth-child("+i+")").append(fieldSet); 
@@ -95,14 +105,15 @@ $(document).ready(function () {
     if(Selectedoption == "Radio Button") {
         var opts = field_options.split(",");
             opts.map(function(index){
-                input = $("<input type=\"radio button\" id=\"" + id+ "\" name=\"" + label + "\" class=\"" + class_name + "\" /> <br>");
+                label = index
+                input = $("<input type='radio'  class='" + class_name + "' /> <br>");
                 fieldSet.append(label);
                 fieldSet.append(input);
                 $("main div:nth-child("+i+")").append(fieldSet);    
             })            
         }
     if(Selectedoption == "upload file"){
-        input = $("<input type=\"upload\" id=\"" + id + "\" name=\"" + label + "\" class=\"" + class_name + "\" placeholder=\"" + field_place + "\" value=\"" + field_value + "\" />");
+        input = $("<input type='file'  class='" + class_name + "' placeholder='" + field_place + "' value='" + field_value + "' />");
             fieldSet.append(label);
             fieldSet.append(input);
             $("main div:nth-child("+i+")").append(fieldSet); 
@@ -112,12 +123,3 @@ $(document).ready(function () {
     });
 
     });
-
-
-
-
-
-
-
-
-
